c3f8f4d98d5169d95987fecdc85ccb675ae74d6b
Jamween <77840853+Jamween@users.noreply.github.com>
2025-04-27 16:16:36 +0100

    Added Add-Subscription form layout and navigation

--- src/app/add-subscription/add-subscription.page.ts
+++ src/app/add-subscription/add-subscription.page.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-subscription',
   templateUrl: './add-subscription.page.html',
   styleUrls: ['./add-subscription.page.scss'],
   standalone: true,
-  imports: [IonicModule, RouterModule, CommonModule],
+  imports: [IonicModule, CommonModule, RouterModule, FormsModule],
 })
 export class AddSubscriptionPage {
-  constructor() {}
+  subscription = {
+    name: '',
+    amount: null,
+    date: '',
+    billingCycle: 'monthly',
+    paymentMethod: ''
+  };
+
+  constructor(private router: Router) {}
+
+  saveSubscription() {
+    console.log('Saving subscription:', this.subscription);
+    // Later we will save to storage
+    this.router.navigate(['/home']);
+  }
 }
